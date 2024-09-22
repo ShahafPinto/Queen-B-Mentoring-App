89bdbd7e7a059bd9223b7de7789bbf1e316a55d1
liorkashi <liorka2@mta.ac.il>
2024-09-22 18:35:01 +0300

    homepage changes

--- app/page.js
+++ app/page.js
@@ -1,14 +1,13 @@
 import Link from "next/link";
 import styles from "./page.module.css";
-import NavBar from "/components/navBar"
-
+import NavBar from "/components/NavBar";
 
 export default function Home() {
-  return (
-    <div>
-        <NavBar />
-        <h1 className={styles.h1}>Welcome to Our Mentorship App</h1>
-        <Link href="/signin">click to Sign In</Link>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <NavBar />
+            <h1 className={styles.h1}>Welcome to Our Mentorship App</h1>
+            <Link href="/signin" className={styles.signInLink}>Click to Sign In</Link>
+        </div>
+    );
 }
