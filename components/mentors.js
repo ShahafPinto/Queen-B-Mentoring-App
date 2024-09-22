c5532777fb36a1f2025ed6cd1924cca75aed488f
rotemmin <rotemminai@gmail.com>
2024-09-22 16:26:21 +0300

    rotem 22.9 16:30

--- components/mentors.js
+++ components/mentors.js
@@ -1,16 +1,19 @@
 import MentorCard from "./mentorCard";
+import styles from "./mentors.module.css";
 
 export default function Mentors() {
     return (
         <div>
             <h1>list of mentors</h1>
-            <div>
-                <MentorCard />
-                <MentorCard />
-                <MentorCard />
-                <MentorCard />
-                <MentorCard />
-                <MentorCard />
+            <div className={styles.mentorsCards}>
+                <div>
+                    <MentorCard />
+                    <MentorCard />
+                    <MentorCard />
+                    <MentorCard />
+                    <MentorCard />
+                    <MentorCard />
+                </div>
             </div>
         </div>
     )
