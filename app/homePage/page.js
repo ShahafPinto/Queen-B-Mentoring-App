6926ad6c27bba2616cc95919cfb1915a5209c414
rotemmin <rotemminai@gmail.com>
2024-09-25 15:41:13 +0300

    rotem 15:40

--- app/homePage/page.js
+++ app/homePage/page.js
@@ -1,4 +1,5 @@
 import Mentors from "@/components/mentors.js";
+import SearchBar from "/components/searchBar.js";
 import styles from "./page.module.css";
 
 export default function Main() {
@@ -6,17 +7,14 @@ export default function Main() {
     <div>
       <h1 className={styles.h1}>Main page</h1>
       <div className={styles.search}>
-        <label for="search">חפשי מנטורית</label>
-        <input type="search" id="search" placeholder="הזיני שם, שפות תכנות או תפקיד..." />
+      <div >
+          <SearchBar/>
+      </div>
+        {/* <label for="search">חפשי מנטורית</label>
+        <input type="search" id="search" placeholder="הזיני שם, שפות תכנות או תפקיד..." /> */}
       </div>
       <div>
         <Mentors />
-        {/* <div className="user-cards">
-          <div className="card">
-            <div className="header">My name</div>
-            <div className="body">email@gmail.com</div>
-          </div>
-        </div> */}
       </div>
     </div>
   );
