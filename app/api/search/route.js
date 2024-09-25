2b1cc334587adca1992e55fbdee2494f6323cf6a
rotemmin <rotemminai@gmail.com>
2024-09-25 16:40:53 +0300

    r

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,30 +1,31 @@
 import query from '../db'; 
 
 export default async function handler(req, res) {
-  const { query2 } = req.query;
-  //  {
-  // const { searchParams } = new URL(req.url);
-  // const searchTerm = searchParams.get('searchTerm');
+  const { searchTerm } = req.query;
 
   try {
     let users;
-    if (query2){
+    if (searchTerm){
       const searchQuery = `
       SELECT * FROM users
-      WHERE LOWER(first_name) LIKE $2 OR
-       LOWER(family_name) LIKE $3 OR EXISTS 
+      WHERE LOWER(first_name) LIKE $1 OR
+       LOWER(family_name) LIKE $1 OR EXISTS 
        (SELECT 1  FROM json_array_elements_text(programing_languages) as pl
-       WHERE LOWER(pl) LIKE $9 ) OR
-       LOWER(job_title) LIKE $11
+       WHERE LOWER(pl) LIKE $1) OR
+       LOWER(job_title) LIKE $1
       `;
 
-      users = await query(searchQuery, [`%${query2.toLowerCase()}%`, `%${query2.toLowerCase()}%`]);
+      users = await query(searchQuery, [`%${searchTerm.toLowerCase()}%`]);
     } else {
       const allMentorsQuery = 'SELECT * FROM users';
       users = await query(allMentorsQuery);
     }
 
-    res.status(200).json(mentors.rows);  // Return the rows from the query result
+    if (!users || !users.rows) {
+      throw new Error('No results returned from database');
+    }
+
+    res.status(200).json(users.rows); 
   } catch (error) {
     console.error('Error querying database:', error);
     res.status(500).json({ error: 'Error fetching mentors' });
